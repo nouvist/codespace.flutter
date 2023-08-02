36908cad84a95de81249d3afbf3f87b741f91be8
nouvist <13176878+nouvist@users.noreply.github.com>
2023-08-02 20:40:14 +0700

    fix: use .profile and .zprofile

--- postinstall.sh
+++ postinstall.sh
@@ -9,10 +9,13 @@ PATH="\$PATH:/opt/android/sdk/platform-tools"
 PATH="\$PATH:/opt/android/sdk/build-tools/34.0.0"
 EOF
 
-echo "source '/home/vscode/.environment'" >> '/home/vscode/.bashrc'
-echo "source '/home/vscode/.environment'" >> '/home/vscode/.zshrc'
+echo 'Add environment to ~/.profile...'
+echo "source '/home/vscode/.environment'" >> '/home/vscode/.profile'
+echo 'Add environment to ~/.zprofile...'
+echo "source '/home/vscode/.environment'" >> '/home/vscode/.zprofile'
 source '/home/vscode/.environment'
 
+echo 'Installing Android Platform 33...'
 yes | sdkmanager 'platform-tools' 'platforms;android-33' 'build-tools;34.0.0'
 flutter --disable-telemetry
 flutter config --android-sdk '/opt/android/sdk'
